d39f6f58fe6547e245cb90a95e8ac76f6025b7af
prashantabalraj <pbalraj@my365.bellevue.edu>
2026-03-20 17:29:22 -0400

    Create BalrajHelloWorld.java

---
+++ Module_1/HelloWorldApp/BalrajHelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String firstName = "YourFirstName";
+        String lastName = "YourLastName";
+        
+        System.out.printf("Hello World from %s %s.%n", firstName, lastName);
+    }
+}
