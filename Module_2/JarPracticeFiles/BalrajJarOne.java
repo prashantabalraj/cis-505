45424cd294e4f3c820df0a523a7452cee8ada070
prashantabalraj <pbalraj@my365.bellevue.edu>
2026-03-26 19:28:32 -0400

    Create BalrajJarOne.java

---
+++ Module_2/JarPracticeFiles/BalrajJarOne.java
@@ -0,0 +1,8 @@
+public class BalrajJarOne {
+    public static void main(String[] args) {
+        String firstName = "Balraj";
+        String lastName = "YourLastName";
+
+        System.out.printf("Hello World from %s %s.%n", firstName, lastName);
+    }
+}
